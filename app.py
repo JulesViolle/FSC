5d719b574c74841f1ccd0e33584e394e4999be05
Zed <96841607+Z3D-9@users.noreply.github.com>
2022-11-26 05:27:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ import requests
 import db
 
 app=Flask(__name__)
+
 @app.route('/')
-def index():
-    return render_template('./Loader/Loader.html')
-@app.route('/home/')
 def home():
     return render_template('./index.html')
 
@@ -279,7 +277,7 @@ def js(path):
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',port=5000,threaded=True)
+    app.run(host='0.0.0.0',port=5000,threaded=True)
 
 
 
