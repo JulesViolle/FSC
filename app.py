ecfefdbf4e91b4e9e2aa3d34cad1d1f60cb83342
Zed <96841607+Z3D-9@users.noreply.github.com>
2022-12-29 12:46:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -156,20 +156,8 @@ def login(User='None',Pass='None'):
                 
                     f=requests.post('https://fsc3301.pythonanywhere.com/login/',data={'T':token}).json()
                 except:
-                    global users_id
-    
-                    userid=request.cookies.get("userID") 
-   
-   
-    
-                    req=requests.post("https://fsc3302.pythonanywhere.com/sadkaidaojd536/token",data={'Token':userid,"Data":"chk"}).json()['message']
-                    print(req)
-                    if req==True:
-                        userid=json.loads(str(pybase64.b64decode(request.cookies.get("userID")).decode()))
-
-                        return login(userid['user'],userid['pass'])
-                    else:
-                        return render_template('./index.html')
+                    
+                       return render_template('./index.html')
                     
            
             if f['message']=='NF' :
