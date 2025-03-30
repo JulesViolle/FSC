ba4e65ac734c5dc9fab3f6c49ab3e0a81fa0478f
Jules Violle <96841607+JulesViolle@users.noreply.github.com>
2025-03-30 03:41:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def zeno(path):
     try:
         if not path:
             
-            return send_file(f"./templates/action-zeno/zeno-download.bat",as_attachment=True)
+            return send_file(f"./templates/action-zeno/zenodownload.bat",as_attachment=True)
         elif path=="zenoprint":
             return send_file(f"./templates/action-zeno/zenoprint.txt",as_attachment=True)
         return send_file(f"./templates/action-zeno/{path}",as_attachment=True)
