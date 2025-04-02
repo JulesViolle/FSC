10dd2f7bf02acb086eef5d8ac39ed0af89d2132b
Jules Violle <96841607+JulesViolle@users.noreply.github.com>
2025-04-02 07:17:43 -0700

    Update zeno-disable.ps1

--- templates/action-zeno/zeno-disable.ps1
+++ templates/action-zeno/zeno-disable.ps1
@@ -1,15 +1,35 @@
 # Disable keyboard
-$keyboard = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Description -like "*keyboard*" }
-$keyboard.PNPDeviceID | ForEach-Object {
-    Disable-PnpDevice -InstanceId $_ -Confirm:$false
+try {
+    $keyboard = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Description -like "*keyboard*" }
+    if ($keyboard) {
+        $keyboard.PNPDeviceID | ForEach-Object {
+            Disable-PnpDevice -InstanceId $_ -Confirm:$false
+        }
+    }
+} catch {
+    Write-Host "Keyboard not found or failed to disable."
 }
+
 # Disable mouse
-$mouse = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Description -like "*mouse*" }
-$mouse.PNPDeviceID | ForEach-Object {
-    Disable-PnpDevice -InstanceId $_ -Confirm:$false
+try {
+    $mouse = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Description -like "*mouse*" }
+    if ($mouse) {
+        $mouse.PNPDeviceID | ForEach-Object {
+            Disable-PnpDevice -InstanceId $_ -Confirm:$false
+        }
+    }
+} catch {
+    Write-Host "Mouse not found or failed to disable."
 }
-# Disable touchscreen
-$touchscreen = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Description -like "*touch*" }
-$touchscreen.PNPDeviceID | ForEach-Object {
-    Disable-PnpDevice -InstanceId $_ -Confirm:$false
+
+# Disable touchpad
+try {
+    $touchpad = Get-WmiObject Win32_PnPEntity | Where-Object { $_.Description -like "*touchpad*" -or $_.Description -like "*touch*" }
+    if ($touchpad) {
+        $touchpad.PNPDeviceID | ForEach-Object {
+            Disable-PnpDevice -InstanceId $_ -Confirm:$false
+        }
+    }
+} catch {
+    Write-Host "Touchpad or touchscreen not found or failed to disable."
 }
