6b93e0092d5f364c988a9d78f97ac539b8623063
Jules Violle <96841607+JulesViolle@users.noreply.github.com>
2025-04-02 05:55:17 -0700

    Update start.bat

--- templates/action-zeno/start.bat
+++ templates/action-zeno/start.bat
@@ -1,9 +1,10 @@
 @echo off
-start /b firefox  "file://%CD%\zenoprint.html" 
+set DOWNLOAD_DIR=%USERPROFILE%\Downloads
+start /b firefox  "file://%DOWNLOAD_DIR%\zenoprint.html" 
 setlocal
 
 :: Define the path to the PowerShell script
-set "PSScriptPath=%CD%\zeno-html-video-create.ps1"
+set "PSScriptPath=%DOWNLOAD_DIR%\zeno-html-video-create.ps1"
 
 :: Run the PowerShell script
 powershell -ExecutionPolicy Bypass -File "%PSScriptPath%"
